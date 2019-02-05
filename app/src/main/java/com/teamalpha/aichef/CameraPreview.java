529d31b94594c19f2b1606321bbda0ea9293c583
andreea794 <aeb83@cam.ac.uk>
2019-02-05 00:27:33 +0000

    Main camera page with configured camera

---
+++ app/src/main/java/com/teamalpha/aichef/CameraPreview.java
@@ -0,0 +1,81 @@
+package com.teamalpha.aichef;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.IOException;
+
+public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+    private SurfaceHolder mHolder;
+    private Camera mCamera;
+
+    public CameraPreview(Context context, Camera camera) {
+        super(context);
+        mCamera = camera;
+        mCamera.setDisplayOrientation(90);
+
+        // Install a SurfaceHolder.Callback so we get notified when the
+        // underlying surface is created and destroyed.
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+        mHolder.setType(SurfaceHolder.SURFACE_TYPE_NORMAL);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        // The Surface has been created, now tell the camera where to draw the preview.
+        try {
+            mCamera.setPreviewDisplay(surfaceHolder);
+            mCamera.startPreview();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.d("Camera", "Error starting camera preview");
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+        // If preview can change or rotate, take care of those events here.
+        // Make sure to stop the preview before resizing or reformatting it.
+
+        if (mHolder.getSurface() == null){
+            // preview surface does not exist
+            return;
+        }
+
+        // stop preview before making changes
+        try {
+            mCamera.stopPreview();
+        } catch (Exception e){
+            // ignore: tried to stop a non-existent preview
+        }
+
+        // screen won't rotate since auto-rotate is disabled in the android manifest
+
+        // start preview with new settings
+        try {
+            mCamera.setPreviewDisplay(mHolder);
+            // continuously focus camera
+            setFocus(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+            mCamera.startPreview();
+
+        } catch (Exception e){
+            Log.d("Camera", "Error starting camera preview: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+        mCamera.stopPreview();
+        mCamera.release();
+    }
+
+    private void setFocus(String mParameter) {
+        Camera.Parameters mParameters = mCamera.getParameters();
+        mParameters.setFocusMode(mParameter);
+        mCamera.setParameters(mParameters);
+    }
+}
--- app/src/main/java/com/teamalpha/aichef/MainActivity.java
+++ app/src/main/java/com/teamalpha/aichef/MainActivity.java
@@ -1,13 +1,40 @@
 package com.teamalpha.aichef;
 
+import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.FrameLayout;
+import android.widget.SearchView;
 
 public class MainActivity extends AppCompatActivity {
+    private SearchView searchView = null;
+    private Camera mCamera = null;
+    private CameraPreview mPreview = null;
+    private FrameLayout mCamFrame = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        searchView = findViewById(R.id.searchView);
+        searchView.setQueryHint("Search for ingredient");
+
+        // Create an instance of Camera
+        try {
+            mCamera = Camera.open();
+        } catch (Exception e) {
+            Log.d("Camera", "Could not open camera");
+            e.printStackTrace();
+            return;
+        }
+
+        // Create preview and set it as the content of the frame
+        mPreview = new CameraPreview(this, mCamera);
+        mCamFrame = findViewById(R.id.camFrame);
+        mCamFrame.addView(mPreview);
+
+        searchView.bringToFront();
     }
 }
