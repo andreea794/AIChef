374083172fd02816970f744366ad0fc39a6761b8
jennazhou <rz296@cam.ac.uk>
2019-02-07 14:15:14 +0000

    Add recipe recommender

--- app/src/main/java/com/teamalpha/aichef/MainActivity.java
+++ app/src/main/java/com/teamalpha/aichef/MainActivity.java
@@ -1,9 +1,16 @@
 package com.teamalpha.aichef;
 
 import android.support.v7.app.AppCompatActivity;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private static RequestQueue mQueue;
+
+
+    RequestQueue queue = (RequestQueue) Volley.newRequestQueue(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
