b0b141ecdc2a3b885d1dff76e071541f3986cedb
Ryxuma <tryfan99@gmail.com>
2019-02-11 23:30:18 +0000

    Actually added interface

--- app/src/main/java/com/teamalpha/aichef/classifier/AIChefClassifier.java
+++ app/src/main/java/com/teamalpha/aichef/classifier/AIChefClassifier.java
@@ -1,4 +1,44 @@
 package com.teamalpha.aichef.classifier;
 
+import java.nio.ByteBuffer;
+import java.util.HashMap;
+import java.util.Map;
+
 public class AIChefClassifier {
+
+    private static final float MIN_OBJ_CHANCE = 0.5f;
+
+    /**
+     * Loads the NN from storage.
+     * May take noticeable time so should be called once (and only once) at startup
+     */
+    public static void loadNetwork() {
+
+    }
+
+    /**
+     *
+     * @param image The XxY RGB image
+     * @return A mapping from each object to it's calculated probability
+     */
+    public static HashMap<String, Float> calculateObjectProbabilities(ByteBuffer image) {
+        return null;
+    }
+
+    /**
+     *
+     * @param image The XxY RGB image
+     * @return Essentially, calculatedObjectProbabilities with objects with probability less than
+     * MIN_OBJ_CHANCE culled
+     */
+    public static HashMap<String, Float> getObjectsInImage(ByteBuffer image) {
+        HashMap<String, Float> objProbs = calculateObjectProbabilities(image);
+        for(String key : objProbs.keySet()) {
+            if(objProbs.get(key) < MIN_OBJ_CHANCE) {
+                objProbs.remove(key);
+            }
+        }
+
+        return objProbs;
+    }
 }
