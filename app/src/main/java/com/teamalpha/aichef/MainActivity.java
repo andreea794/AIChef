d3e573d3bcc7559ac995aa79b0d2f329c1a15a41
aaront97 <aaron.t97@gmail.com>
2019-02-13 15:37:19 +0000

    changed gitignore

--- app/src/main/java/com/teamalpha/aichef/MainActivity.java
+++ app/src/main/java/com/teamalpha/aichef/MainActivity.java
@@ -3,16 +3,21 @@ package com.teamalpha.aichef;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final RequestQueue queue = Volley.newRequestQueue(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button recipesListBtn = (Button)findViewById(R.id.recipesListBtn);
