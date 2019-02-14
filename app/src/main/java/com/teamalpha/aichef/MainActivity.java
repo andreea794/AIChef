34c17c2c585ddf2ebb1b5dd538b826396a3c9e9d
Joon Hyuk Jeong <andytross1022@gmail.com>
2019-02-14 21:39:56 +0000

    Add PanelSlideListener to control camera feed

--- app/src/main/java/com/teamalpha/aichef/MainActivity.java
+++ app/src/main/java/com/teamalpha/aichef/MainActivity.java
@@ -5,7 +5,10 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
+import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 import com.teamalpha.aichef.slideuppanel.IngredientPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,10 +19,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //TODO: Save the scanned ingredients state when you transition to other activity.
+        SlidingUpPanelLayout slidingUpPanelLayout = findViewById(R.id.layout_slidinguppanel);
+        slidingUpPanelLayout.addPanelSlideListener(new SlidingUpPanelListener());
+
+
         ViewPager vp = findViewById(R.id.vpPager);
         adapter = new IngredientPagerAdapter(getSupportFragmentManager());
         TabLayout tabLayout = findViewById(R.id.tab_layout);
         tabLayout.setupWithViewPager(vp);
         vp.setAdapter(adapter);
+    }
+
+    /**
+     * Simple PanelSlideListener which implements camera feed pausing when the slide up panel is
+     * expanded.
+     */
+    private class SlidingUpPanelListener implements SlidingUpPanelLayout.PanelSlideListener {
+        @Override
+        public void onPanelSlide(View panel, float slideOffset) {
+        }
+
+        @Override
+        public void onPanelStateChanged(View panel, SlidingUpPanelLayout.PanelState previousState, SlidingUpPanelLayout.PanelState newState) {
+            //TODO: Pause and resume camera based on the previous state
+            //In the following line, the Toast is only shown when you expand the panel.
+            if (previousState == SlidingUpPanelLayout.PanelState.COLLAPSED)
+                Toast.makeText(MainActivity.this, "Panel Slide", Toast.LENGTH_LONG).show();
+        }
     }
 }
