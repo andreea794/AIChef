1ff2e2d5f04e62351ec6e4ffff33c2296db74ec9
andreea794 <aeb83@cam.ac.uk>
2019-02-17 21:04:26 +0000

    Configure preview to take frames every second

--- app/src/main/java/com/teamalpha/aichef/MainActivity.java
+++ app/src/main/java/com/teamalpha/aichef/MainActivity.java
@@ -2,23 +2,29 @@ package com.teamalpha.aichef;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
-import android.hardware.Camera;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.SearchView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CameraPreview.PreviewListener{
     private SearchView searchView = null;
-    private Camera mCamera = null;
-    private CameraPreview mPreview = null;
+    private CameraPreview mPreview;
+    private boolean isPaused = false; // whether the preview is paused
     private FrameLayout mCamFrame = null;
     private static final int CAMERA_REQUEST_ID = 1;
 
+    private Button b;
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,29 +33,21 @@ public class MainActivity extends AppCompatActivity {
         searchView = findViewById(R.id.searchView);
         mCamFrame = findViewById(R.id.camFrame);
 
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
-                // Have been denied permission before, so show user an explanation as to why we
-                // need camera permission
-                Snackbar explanation = Snackbar.make(
-                        mCamFrame,
-                        "This app needs camera permission in order to scan your ingredients.",
-                        Snackbar.LENGTH_LONG);
-                explanation.show();
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.CAMERA},
-                        CAMERA_REQUEST_ID);
-            } else {
-                // No explanation needed; request the permission
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.CAMERA},
-                        CAMERA_REQUEST_ID);
+        b = findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pauseOrResumeCamera();
             }
-        }
+        });
+
+        askCameraPermission();
+
         // Permission has been granted
         initialiseState();
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
     @Override
     public void onRequestPermissionsResult(
             int requestCode, String permissions[], int[] grantResults) {
@@ -70,28 +68,58 @@ public class MainActivity extends AppCompatActivity {
                     explanation.show();
                 }
             }
-
             // other 'case' lines to check for other
             // permissions this app might request.
         }
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
     private void initialiseState() {
         searchView.setQueryHint("Search for ingredient");
 
-        // Create an instance of Camera
-        try {
-            mCamera = Camera.open();
-        } catch (Exception e) {
-            Log.d("CAMERA", "Could not open camera");
-            e.printStackTrace();
-            return;
-        }
-
         // Create preview and set it as the content of the frame
-        mPreview = new CameraPreview(this, mCamera);
+        mPreview = new CameraPreview(this);
         mCamFrame.addView(mPreview);
 
         searchView.bringToFront();
+    }
+
+    private void askCameraPermission() {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+                // Have been denied permission before, so show user an explanation as to why we
+                // need camera permission
+                Snackbar explanation = Snackbar.make(
+                        mCamFrame,
+                        "This app needs camera permission in order to scan your ingredients.",
+                        Snackbar.LENGTH_LONG);
+                explanation.show();
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CAMERA},
+                        CAMERA_REQUEST_ID);
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CAMERA},
+                        CAMERA_REQUEST_ID);
+            }
+        }
+    }
+
+    @Override
+    public void onPreviewUpdated(byte[] data, int width, int height) {
+        if (data != null) {
+            /* TODO: send image data to back-end. */
+            if (isPaused)
+                Log.e("PREVIEW", "Paused, but still in preview!");
+        }
+    }
+
+    public void pauseOrResumeCamera() {
+        isPaused = !isPaused;
+        mPreview.pauseOrResume(isPaused);
+        if (!isPaused) {
+            mPreview.resetBuffer();
+        }
     }
 }
