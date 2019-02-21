b6bb287d74793f28f7e573afbc5d28fa4eea7084
aaront97 <33289351+aaront97@users.noreply.github.com>
2019-02-21 13:22:45 +0000

    added images, recipe url intent, shopping list

--- app/src/main/java/com/teamalpha/aichef/RecipesList.java
+++ app/src/main/java/com/teamalpha/aichef/RecipesList.java
@@ -1,5 +1,6 @@
 package com.teamalpha.aichef;
 
+import android.content.Intent;
 import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,60 +9,93 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.LinkedList;
+import java.util.List;
+
+import api.GetRecipeList;
+import api.GetSelectedRecipeData;
+import api.Ingredient;
+import api.Recipe;
 
 public class RecipesList extends AppCompatActivity {
     ListView recipesListView;
-    LinkedList<RecipeItem> recipesList;
-    LinkedList<RecipeItem> selectedRecipes;
+    //List<Recipe> recipesList;
+    List<Ingredient> ingredientList;
+    boolean ingredientListAPICalled = false;
+    static RecipesListAdapter recipesListAdapter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final RequestQueue queue = Volley.newRequestQueue(this);
+        RecipeRequestFinishedListener listener = new RecipeRequestFinishedListener();
+        listener.setQueue(queue);
+        queue.addRequestFinishedListener(listener);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipes_list);
-
         Resources res = getResources();
+        ingredientList = new LinkedList<Ingredient>();
 
         recipesListView = (ListView)findViewById(R.id.recipesList);
+        recipesListAdapter = new RecipesListAdapter(getApplicationContext(), res);
+        recipesListView.setAdapter(recipesListAdapter);
+
+
 
         if(getIntent().hasExtra("recipes")){
-            recipesList = (LinkedList)getIntent().getExtras().get("recipes");
+//            ArrayList<Recipe> temp = getIntent().getExtras().getParcelableArrayList("recipes");
+//            recipesList = temp.stream()
+
         }
         else{
-            recipesList = initialiseTestList();
+            List<Recipe> recipeList = new LinkedList<Recipe>();
+            recipeList.add(new Recipe("Easy & Delish! ~ Apple Crumble", "641803", "https://spoonacular.com/recipeImages/Easy---Delish--Apple-Crumble-641803.jpg"));
+            RecipesListAdapter.recipesList = recipeList;
+            GetSelectedRecipeData.callIngredientsListAPI(recipeList, queue, ingredientList);
         }
-
-        final RecipesListAdapter recipesListAdapter = new RecipesListAdapter(getApplicationContext(), recipesList, res);
-        recipesListView.setAdapter(recipesListAdapter);
 
         Button generateIngredientsBtn  = (Button)findViewById(R.id.spawnIngredientsList);
         generateIngredientsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ArrayList<RecipeItem> recipes = recipesListAdapter.getPickedRecipeNames();
-                Collections.sort(recipes, new Comparator<RecipeItem>() {
-                    @Override
-                    public int compare(RecipeItem recipeItem, RecipeItem t1) {
-                        return recipeItem.getRecipeText().compareTo(t1.getRecipeText());
-                    }
-                });
-                for(RecipeItem recipe : recipes){
-                    System.out.println(recipe.getRecipeText());
-                    System.out.flush();
+                ArrayList<String> ingredients = new ArrayList<String>();
+                for(Ingredient ingredient : ingredientList){
+                    ingredients.add(ingredient.getName());
                 }
+                Intent shoppingList = new Intent(getApplicationContext(), ShoppingActivity.class);
+                shoppingList.putStringArrayListExtra("Ingredients", ingredients);
+                startActivity(shoppingList);
+                //TODO: Convert to parcelable
             }
         });
 
+
     }
 
-    private static LinkedList<RecipeItem> initialiseTestList(){
-        LinkedList<RecipeItem> testList = new LinkedList<RecipeItem>();
-        for(int i = 0; i < 20; i++){
-            RecipeItem recipe = new RecipeItem(R.drawable.pepe, "Item: " + i, null);
-            testList.add(recipe);
+
+    //FOR TESTING
+    private class RecipeRequestFinishedListener implements RequestQueue.RequestFinishedListener<JsonArrayRequest>{
+
+        RequestQueue queue = null;
+        public void setQueue(RequestQueue queue){
+            this.queue = queue;
         }
-        return testList;
+        @Override
+        public void onRequestFinished(Request<JsonArrayRequest> request){
+            recipesListAdapter.notifyDataSetChanged();
+            if(ShoppingActivity.adapter != null){
+                recipesListAdapter.notifyDataSetChanged();
+            }
+        }
+
     }
+
 }
--- app/src/main/java/com/teamalpha/aichef/RecipesListAdapter.java
+++ app/src/main/java/com/teamalpha/aichef/RecipesListAdapter.java
@@ -1,46 +1,64 @@
 package com.teamalpha.aichef;
 
 import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.CheckedTextView;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import api.Recipe;
 
 public class RecipesListAdapter extends BaseAdapter {
 
-    private LinkedList<RecipeItem> recipesList;
+    static List<Recipe> recipesList;
     private Context c;
     private LayoutInflater mInflater;
     private Resources res;
     private TypedArray tickImage;
-    private ArrayList<Boolean> checkedRecipes;
-    private ArrayList<RecipeItem> pickedRecipeNames;
+    static List<Boolean> checkedRecipes;
+    private List<Recipe> pickedRecipeNames;
+    ExecutorService executor;
 
 
-
-    public ArrayList<RecipeItem> getPickedRecipeNames(){
+    public List<Recipe> getPickedRecipeNames(){
         return pickedRecipeNames;
     }
 
-    public RecipesListAdapter(Context c, LinkedList<RecipeItem> recipesList, Resources res){
+    public RecipesListAdapter(Context c, Resources res){
         this.c = c;
-        this.recipesList = recipesList;
         this.mInflater = (LayoutInflater)c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         this.res = res;
         tickImage = this.res.obtainTypedArray(R.array.tickImages);
-        this.checkedRecipes = new ArrayList<Boolean>(recipesList.size());
-        this.pickedRecipeNames = new ArrayList<RecipeItem>();
-        for(int i = 0; i<recipesList.size(); i++){
-            checkedRecipes.add(false);
-        }
+        this.recipesList = new LinkedList<Recipe>();
+        this.checkedRecipes = new LinkedList<Boolean>();
+        this.pickedRecipeNames = new LinkedList<Recipe>();
+        this.executor = Executors.newFixedThreadPool(5);
+
     }
 
     @Override
@@ -65,6 +83,8 @@ public class RecipesListAdapter extends BaseAdapter {
 
     @Override
     public View getView(final int i, View view, ViewGroup viewGroup) {
+
+
         View v;
         if(view == null){
             v = mInflater.inflate(R.layout.recipes_list_layout, null);
@@ -72,37 +92,102 @@ public class RecipesListAdapter extends BaseAdapter {
         else{
             v = view;
         }
-        RecipeItem recipe = recipesList.get(i);
+
+        final Recipe recipe = recipesList.get(i);
         final ImageView recipeImg= (ImageView)v.findViewById(R.id.recipeImg);
         final CheckedTextView recipeText = (CheckedTextView)v.findViewById(R.id.recipeText);
-        recipeText.setText(recipesList.get(i).getRecipeText());
-        recipeImg.setImageResource(recipe.getRecipeImg());
-        //make sure recipe is consistent with Current View as determined
-        recipeText.setChecked(checkedRecipes.get(i));
-        if(recipeText.isChecked()){
-            recipeText.setCheckMarkDrawable(R.drawable.checked);
+        String web = recipe.getRecipeImageLink();
+        if(web == null){
+            return v;
+        }
+        String[] substring = web.split("\\.");
+        String file_format = substring[substring.length-1];
+        //File imgFile = new File(System.getProperty("user.dir") + "\\ImageResource\\" + recipe.getRecipeID()+"."+file_format);
+        ContextWrapper contextWrapper = new ContextWrapper(c);
+        File directory = contextWrapper.getDir(c.getFilesDir().getName(), Context.MODE_PRIVATE);
+        File imgFile =  new File(directory, recipe.getRecipeID() + file_format);
+        try{
+            if(!imgFile.exists()){
+                imgFile.createNewFile();
+                executor.execute(new Runnable() {
+                    public void run() {
+                        try {
+                            String web = recipe.getRecipeImageLink();
+                            String[] substring = web.split("\\.");
+                            String file_format = substring[substring.length - 1];
+                            ContextWrapper contextWrapper = new ContextWrapper(c);
+                            File directory = contextWrapper.getDir(c.getFilesDir().getName(), Context.MODE_PRIVATE);
+                            File imgFile = new File(directory, recipe.getRecipeID() + file_format);
+                            URL url = new URL(web);
+                            System.setProperty("http.agent", "chrome");
+                            InputStream in = new BufferedInputStream(url.openStream());
+                            OutputStream out = new BufferedOutputStream(new FileOutputStream(imgFile));
+                            for (int j; (j = in.read()) != -1; ) {
+                                out.write(j);
+                            }
+                            in.close();
+                            out.close();
+                            notifyDataSetChanged();
+                        }
+                        catch(Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+        recipeImg.setImageBitmap(myBitmap);
+        recipeText.setText(recipesList.get(i).getRecipeName());
+        final String url = recipe.getRecipeURL();
+        Button moreButton = (Button)v.findViewById(R.id.moreButton);
+        if(url == null){
+            moreButton.setEnabled(false);
         }
         else{
-            recipeText.setCheckMarkDrawable(0);
+            moreButton.setEnabled(true);
         }
-        //by checkedRecipes
-        recipeText.setOnClickListener(new View.OnClickListener() {
+        moreButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(checkedRecipes.get(i)){
-                    recipeText.setCheckMarkDrawable(0);
-                    recipeText.setChecked(false);
-                    checkedRecipes.set(i, false);
-                    pickedRecipeNames.remove(recipesList.get(i)); //remove from pickedRecipes
-                }
-                else {
-                    recipeText.setCheckMarkDrawable(R.drawable.checked);
-                    recipeText.setChecked(true);
-                    checkedRecipes.set(i, true);
-                    pickedRecipeNames.add(recipesList.get(i)); //add to pickedRecipes
-                }
+                Intent i = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse(url));
+                c.startActivity(i);
             }
         });
+
+
+        //LEFT OUT FOR NOW
+//        recipeImg.setImageResource(recipe.getRecipeImageRes());
+        //make sure recipe is consistent with Current View as determined
+        //recipeText.setChecked(checkedRecipes.get(i));
+//        if(recipeText.isChecked()){
+//            recipeText.setCheckMarkDrawable(R.drawable.checked);
+//        }
+//        else{
+//            recipeText.setCheckMarkDrawable(0);
+//        }
+//        //by checkedRecipes
+//        recipeText.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                if(checkedRecipes.get(i)){
+//                    recipeText.setCheckMarkDrawable(0);
+//                    recipeText.setChecked(false);
+//                    checkedRecipes.set(i, false);
+//                    pickedRecipeNames.remove(recipesList.get(i)); //remove from pickedRecipes
+//                }
+//                else {
+//                    recipeText.setCheckMarkDrawable(R.drawable.checked);
+//                    recipeText.setChecked(true);
+//                    checkedRecipes.set(i, true);
+//                    pickedRecipeNames.add(recipesList.get(i)); //add to pickedRecipes
+//                }
+//            }
+//        });
         return v;
     }
 }
