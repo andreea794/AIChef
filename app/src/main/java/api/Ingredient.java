750d59a4fce342775719a02b55a1de2ebfb698a0
Joon Hyuk Jeong <andytross1022@gmail.com>
2019-02-25 13:17:19 +0000

    Fix Ingredient access bug

--- app/src/main/java/api/Ingredient.java
+++ app/src/main/java/api/Ingredient.java
@@ -4,7 +4,7 @@ import android.os.Parcel;
 import android.os.Parcelable;
 
 public class Ingredient implements Parcelable {
-      final private String name;
+    final private String name;
 //    final private String amount;
 //    final private String unit;
 //    final private String instruction;
