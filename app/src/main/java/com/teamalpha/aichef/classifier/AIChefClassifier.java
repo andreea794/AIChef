96671de71cedeef9087b5f70e9e1f10f507bb232
Ryxuma <tryfan99@gmail.com>
2019-03-04 11:11:52 +0000

    Removed image printing

--- app/src/main/java/com/teamalpha/aichef/classifier/AIChefClassifier.java
+++ app/src/main/java/com/teamalpha/aichef/classifier/AIChefClassifier.java
@@ -247,8 +247,6 @@ public class AIChefClassifier implements Runnable {
 //            Log.e("AIChefClassifier", "Error saving img");
 //        }
 
-        String s = "";
-
         float[][][][] inputImage = new float[1][INPUT_SIZE][INPUT_SIZE][3];
         for(int x = 0; x < INPUT_SIZE; x++) {
             for(int y = 0; y < INPUT_SIZE; y++) {
@@ -257,12 +255,9 @@ public class AIChefClassifier implements Runnable {
                 inputImage[0][x][y][1] = ((pixel >> 8) & 0xff) / 255f;
                 inputImage[0][x][y][2] = ((pixel >> 0) & 0xff) / 255f;
 
-                s += x + " " + y + " " + pixel + "\n";
             }
         }
-
-        Log.i("AIChefClassifier", s);
-
+        
 
 
         // Run network
